a60a4d36e2343b5e49aac01ee27fa6df60958bfb
Dmitry Uvarov <diman_niks@mail.ru>
2023-04-20 10:39:56 +0300

    feat: change mode

--- src/components/mode/Mode.tsx
+++ src/components/mode/Mode.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useAppSelector } from '../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { ModeId } from './types';
+import { changeMode } from '../../store/features/calculatorSlice';
 import IconEye from '../../../public/icons/eye.svg';
 import IconSelector from '../../../public/icons/selector.svg';
 import ButtonIcon from '../ui/buttons/icon/ButtonIcon';
@@ -9,14 +10,19 @@ import styles from './Mode.module.scss';
 
 const Mode: React.FC = () => {
   const { mode } = useAppSelector((state) => state.calculator);
+  const dispatch = useAppDispatch();
+
+  const handleClick = (newMode: ModeId) => {
+    dispatch(changeMode(newMode));
+  };
 
   return (
     <div className={styles.mode}>
-      <ButtonIcon isActive={mode === ModeId.runtime}>
+      <ButtonIcon isActive={mode === ModeId.runtime} onClick={() => handleClick(ModeId.runtime)}>
         <IconEye />
         <span>Runtime</span>
       </ButtonIcon>
-      <ButtonIcon isActive={mode === ModeId.constructor}>
+      <ButtonIcon isActive={mode === ModeId.constructor} onClick={() => handleClick(ModeId.constructor)}>
         <IconSelector />
         <span>Constructor</span>
       </ButtonIcon>
--- src/components/ui/buttons/icon/ButtonIcon.tsx
+++ src/components/ui/buttons/icon/ButtonIcon.tsx
@@ -6,10 +6,15 @@ import styles from './ButtonIcon.module.scss';
 type Props = {
   children: ReactNode;
   isActive?: boolean;
+  onClick?: () => void;
 };
 
-const ButtonIcon: React.FC<Props> = ({ children, isActive }) => {
-  return <button className={cx(styles.button, { [styles.button_active]: isActive })}>{children}</button>;
+const ButtonIcon: React.FC<Props> = ({ children, isActive, onClick }) => {
+  return (
+    <button className={cx(styles.button, { [styles.button_active]: isActive })} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default ButtonIcon;
